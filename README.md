75dc255883a53a5c9745e277ace36588e0f40832
PolatB <burakpolat4141@gmail.com>
2022-09-15 20:12:33 +0300

    readme.md added

---
+++ README.md
@@ -0,0 +1,12 @@
+In this project basics of Html/Css and JavaScript used. This project is build to make some practices of the skills that I have. There is a gif below that display the project.
+
+Used Elements;
+-Google font family: Poppins,
+-Css: basic Css,
+-JavaScript: addEventListener, createElement, add, innerText, appendChild, forEach, pause functions.
+
+Project definition;
+
+There are buttons on the top of the screen and when you click on them, they respond different sound effects.
+
+![Alt Text](https://media.giphy.com/media/Br4g7RU323dgUsKIi4/giphy.gif)
---
+++ script.js
@@ -0,0 +1,32 @@
+const sounds = [
+    "applause",
+    "boo",
+    "gasp",
+    "success",
+    "tadaa",
+    "wrong"
+];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+        stopSounds();
+
+        document.getElementById(sound).play();
+    })
+
+    document.body.appendChild(btn);
+
+});
+
+function stopSounds() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0
+    })
+}
